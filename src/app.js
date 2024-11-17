78308195a5274dd450c107dc7b07001d4a32e70c
theparkerbrother <forrest273@gmail.com>
2024-11-17 01:52:52 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,50 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = [
+    "The dog",
+    "My grandma",
+    "The mailman",
+    "My bird",
+    "My brother Harrison",
+    "The carpet cleaner",
+    "Sarah",
+    "Kevin Hart",
+    "Kevin James"
+  ];
+  let action = [
+    "ate",
+    "peed",
+    "crushed",
+    "broke",
+    "nuked",
+    "fire bombed",
+    "sued",
+    "twirled"
+  ];
+  let what = [
+    "my homework",
+    "my phone",
+    "the car",
+    "my best friend",
+    "my breakfast",
+    "trash can",
+    "my stapler"
+  ];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoNum = Math.floor(Math.random() * who.length);
+  let actionNum = Math.floor(Math.random() * action.length);
+  let whatNum = Math.floor(Math.random() * what.length);
+  let whenNum = Math.floor(Math.random() * when.length);
+
+  document.getElementById(
+    "excuse"
+  ).innerHTML = `${who[whoNum]} ${action[actionNum]} ${what[whatNum]} ${when[whenNum]}!`;
 };
